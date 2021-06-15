b2ca5f2ced5d7c0a81f8eed3fb34d2e1d7ddc6de
Nathan Huse <nathan.huse@yahoo.com>
2021-06-15 21:29:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -146,8 +146,10 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
-  
-  rock.style.left = `${top - 4}px`;
+  var leftNumbers = DODGER.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+  if()
+  rock.style.left = `${left - 4}px`;
   
    
 }
